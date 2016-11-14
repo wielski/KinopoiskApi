142483e9b20cec80e47f924374a2472e903083fa
Beloo <Awful.Vi@yandex.ru>
2016-11-14 16:45:52 +0200

    * java part updated to support the newest kinopoisk api * except several api's like `getKPTodayFilms`

---
+++ src/java/com/kinopoisk/Constants.java
@@ -0,0 +1,6 @@
+package com.kinopoisk;
+
+public final class Constants {
+    public static final String API_VERSION = "3.11.0";
+    public static final String KINOPOISK_ENDPOINT = "https://ext.kinopoisk.ru/ios/";
+}
---
+++ src/java/com/kinopoisk/NetworkApiFactory.java
@@ -0,0 +1,76 @@
+package com.kinopoisk;
+
+import android.content.Context;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.core.Version;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+
+import java.net.CookieManager;
+import java.net.CookiePolicy;
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.Cache;
+import okhttp3.JavaNetCookieJar;
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
+import retrofit2.converter.jackson.JacksonConverterFactory;
+
+public class NetworkApiFactory {
+
+    OkHttpClient provideHttpClient(Context context) {
+
+        // Enable caching for OkHttp
+        int cacheSize = 10 * 1024 * 1024; // 10 MiB
+        Cache cache = new Cache(context.getCacheDir(), cacheSize);
+
+        OkHttpClient.Builder builder = new OkHttpClient().newBuilder()
+                .cache(cache)
+                .connectTimeout(30, TimeUnit.SECONDS);
+
+
+        builder.addInterceptor(new KinopoiskRequestInterceptor());
+
+        //logging interceptor should be last interceptor in chain
+        HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor();
+        loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+
+        builder.addInterceptor(loggingInterceptor);
+
+        CookieManager cookieManager = new CookieManager();
+        cookieManager.setCookiePolicy(CookiePolicy.ACCEPT_ALL);
+        builder.cookieJar(new JavaNetCookieJar(cookieManager));
+
+        return builder.build();
+    }
+
+    ObjectMapper provideObjectMapper() {
+        final SimpleModule module = new SimpleModule("", Version.unknownVersion());
+
+        //jackson object mapper setup
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(module);
+        objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+
+        // don not fail while unknown json props on release version
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+
+        return objectMapper;
+    }
+
+    JacksonConverterFactory provideJacksonConverterFactory(ObjectMapper objectMapper) {
+        return JacksonConverterFactory.create(objectMapper);
+    }
+
+    Retrofit provideRetrofitAdapter(OkHttpClient client, JacksonConverterFactory factory) {
+        return new Retrofit.Builder()
+                .baseUrl(Constants.KINOPOISK_ENDPOINT + Constants.API_VERSION + "/")
+                .client(client)
+                .addConverterFactory(factory)
+                .build();
+    }
+}
